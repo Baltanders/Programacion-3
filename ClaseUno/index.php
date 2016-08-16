b0fa1c37b90117180f01d391d400dc571c7a13a7
Baltanders <baltanders66@gmail.com>
2016-08-16 20:06:58 -0300

    Prueba

---
+++ ClaseUno/index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title>Leipold</title>
+</head>
+<body>
+	<h1>Hola Mundo</h1>
+<?php
+	echo "Hola PHP";
+	echo "<h2> Hola PHP</H2>";
+
+	$numero=10;
+	//echo "El numero es ".$numero;
+	//echo "El numero es $numero";
+	echo 'El numero es $numero';
+?>
+</body>
+</html>
