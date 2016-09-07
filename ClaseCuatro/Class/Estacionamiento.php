b5104299d14c0d343653b9e68518ebeb4a5bf23a
Baltanders <baltanders66@gmail.com>
2016-09-06 21:41:54 -0300

    cLASE 4 Lectura del archivo

--- ClaseCuatro/Class/Estacionamiento.php
+++ ClaseCuatro/Class/Estacionamiento.php
@@ -14,12 +14,30 @@ class Estacionamiento
 	{
 		echo("<br>llega");
 		$fecha=date("Y-m-d H:i:s");
-		$renglon="$patente"." - $fecha"."\n";
+		$renglon="$patente"."<=>"."$fecha"."\n";
 		$miarchivo=fopen("Estacionado.txt", "a");
 		fwrite($miarchivo, $renglon);
 		fclose($miarchivo);
 
 	}
 
+	public static function Leer()
+	{
+		$ListadoAutos = array();
+		$miarchivo=fopen("Estacionado.txt", "r");
+		while (!(feof($miarchivo))) {
+			$renglon=fgets($miarchivo);
+			//echo "<br>$renglon";
+			$auto=explode("<=>", $renglon);
+			//echo "$auto[0] $auto[1] <br>";
+			if ($auto[0]!="") {
+				$ListadoAutos[]=$auto;
+			}
+			
+		}
+		fclose($miarchivo);
+		return $ListadoAutos;
+	}
+
 }
- ?>
+?>
--- ClaseCuatro/nexo.php
+++ ClaseCuatro/nexo.php
@@ -3,12 +3,21 @@
 	$patente=$_POST['patente'];
 	$accion=$_POST['accion'];
 
-	echo "Pantente: $patente <br>";
-	echo "Accion: $accion";
+	//echo "Pantente: $patente <br>";
+	//echo "Accion: $accion";
 
-	header("location:index.php");
+	//header("location:index.php");
 	if ($accion=="Estacionar") {
+		echo "Pantente: $patente <br>";
+		echo "Accion: $accion";
 		Estacionamiento::Guardar($patente);
 	}
+	else
+	{
+		$Listado= array();
+		echo "Accion: $accion <br>";
+		$Listado=Estacionamiento::Leer();
+		var_dump($Listado);
 
+	}
  ?>
